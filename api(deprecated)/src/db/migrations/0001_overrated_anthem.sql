0602d20fa6dc58ec76d89894f2bcaaafbe115010
Yu-Chun Ku <yuchun.ku@medivis.com>
2026-01-23 20:26:19 -0500

    deleted old backend

--- api(deprecated)/src/db/migrations/0001_overrated_anthem.sql
+++
@@ -1,2 +0,0 @@
-ALTER TABLE "users" ADD COLUMN "personal_org_id" text;--> statement-breakpoint
-CREATE INDEX "users_personal_org_idx" ON "users" USING btree ("personal_org_id");
--- api(deprecated)/src/middleware/index.ts
+++
@@ -1,3 +0,0 @@
-export * from "./auth.js";
-export * from "./device.js";
-export * from "./error.js";
